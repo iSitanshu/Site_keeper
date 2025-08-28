6066ccd14975021fa654f99565bc4a4d964b3267
iSitanshu <Sitanshumishra18@gmail.com>
2025-08-28 10:49:38 +0530

    adding configeration in next.config.ts file

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,22 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
+
+// // next.config.js
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+  
+// };
+
+// module.exports = nextConfig;
