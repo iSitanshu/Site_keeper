ba0b16c58e25d7a54cc69d8a9638f2601df5697d
iSitanshu <Sitanshumishra18@gmail.com>
2025-08-28 10:53:09 +0530

    Changes in next.config.ts file

--- next.config.ts
+++ next.config.ts
@@ -1,23 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   images: {
     remotePatterns: [
       {
         protocol: "https",
         hostname: "res.cloudinary.com",
+        pathname: "**", // allow all paths
+      },
+      {
+        protocol: "http",
+        hostname: "res.cloudinary.com",
+        pathname: "**",
       },
     ],
   },
 };
 
 export default nextConfig;
-
-// // next.config.js
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-  
-// };
-
-// module.exports = nextConfig;
